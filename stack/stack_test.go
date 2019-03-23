d8c8ef7bc5d7596999e47de10370a0be0cc04390
baybird <bayareabird@gmail.com>
2019-03-22 18:17:25 -0700

    New testcases for Pop().

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -16,14 +16,24 @@ func TestStack(t *testing.T) {
 
 	// test push method
 	stack.Push(3.1415)
+	stack.Push(1)
 	stack.Push("P")
-	if stack.Len() != 2 {
+	if stack.Len() != 3 {
 		t.Errorf("Push() method failed. Stack length should be 2.")
 	}
 
 	// test pop up last element
 	e := stack.Pop()
-	if e.(string) != "P" || stack.Len() != 1 {
-		t.Errorf("Pop() method failed.")
+	if e.(string) != "P" {
+		t.Errorf("Pop() method failed. Element should be \"P\"")
+	}
+
+	e = stack.Pop()
+	if e.(int) != 1 {
+		t.Errorf("Pop() method failed. It should be 1.")
+	}
+
+	if stack.Len() != 1 {
+		t.Errorf("Pop() method failed. Length of stack should 1 now.")
 	}
 }
